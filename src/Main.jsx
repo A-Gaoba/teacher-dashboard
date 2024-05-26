1420c47e3149ec605c0d60675d7ab053a9449eeb
a-goaba <abdulrahman1997abdo@gmail.com>
2024-05-26 05:49:38 +0500

    fixed

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,5 +1,11 @@
-import reactDom from "react-dom";
-import App from "./App";
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import App from "./App.jsx";
 import "./index.css";
 
-reactDom.render(<App />, document.getElementById("root"));
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
